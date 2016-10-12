f14c48826cbc9f9f544a8fd5d3b7757d51c712b0
Toxagin <toxagin@gmail.com>
2016-10-12 19:46:30 +0300

    Create module lang_frequency.py. It provids 3 functions load_data(filepath), get_most_frequent_words(text), show_words(data)

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,27 @@
-
+import re
+from collections import defaultdict
 
 def load_data(filepath):
-    pass
+    data = None
+    with open(filepath, 'r') as file:
+        data = file.read()
+    return data
 
 
 def get_most_frequent_words(text):
-    pass
+    words = defaultdict(int) # provide initial value for new keys 
+    for word in re.split(r'\W+', text):
+    	words[word] += 1
+    return sorted(words.items(), key = lambda x: x[1], reverse = True)[:10]
+
+
+def show_words(data):
+	for position, (x, y) in enumerate(data, start = 1):
+		print('{2} "{0}": {1}'.format(x, y, position))
 
 
 if __name__ == '__main__':
-    pass
+    words = get_most_frequent_words(
+    	load_data(input('Введите путь к файлу: '))
+    )
+    show_words(words)
