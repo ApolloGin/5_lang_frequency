e17400f317fd37113f28492bd7518dfb4132596a
Toxagin <toxagin@gmail.com>
2016-10-16 19:19:12 +0300

    Fix function get_most_frequent_words. Now it uses collections.Counter. Readme file now more readability

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # 5_lang_frequency
-Скрипт определяет частоту упоминания слов в тексте.
-Для начала работы скрипта необходимо запустить lang_frequency.py. После этого
-будет предложено ввести путь к файлу с текстом. На выходе получаем Список из
-десяти слов наиболее часто встречающихся в указаном тексте.
+
+Simple script, which allows you to find the ten most common words in text file
+
+# Quick start
+
+ - Run lang_frequency.py '>>>Python3.5 lang_frequency.py'
+ - **Input**: text file path
+ - **Output**: ten most frequent words in there
+
+# Module functions
+
+ - 'load_data(filepath)' - loads text from file specifies in filepath
+ - 'get_most_frequent_words(text)' - get ten most common wors in text. Return list of tuples (word, count)
+ - 'show_words(data)' - pritty-print data which is list of tuples (word, count)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,27 +1,25 @@
 import re
-from collections import defaultdict
+from collections import Counter
 
 def load_data(filepath):
     data = None
-    with open(filepath, 'r') as file:
-        data = file.read()
+    with open(filepath, 'r') as file_with_text:
+        data = file_with_text.read()
     return data
 
 
 def get_most_frequent_words(text):
-    words = defaultdict(int) # provide initial value for new keys 
-    for word in re.split(r'\W+', text):
-    	words[word] += 1
-    return sorted(words.items(), key = lambda x: x[1], reverse = True)[:10]
+    words = re.findall(r'\w+', text.lower())
+    return Counter(words).most_common(10)
 
 
 def show_words(data):
-	for position, (x, y) in enumerate(data, start = 1):
-		print('{2} "{0}": {1}'.format(x, y, position))
+    for position, (x, y) in enumerate(data, start = 1):
+        print('{2} "{0}": {1}'.format(x, y, position))
 
 
 if __name__ == '__main__':
     words = get_most_frequent_words(
-    	load_data(input('Введите путь к файлу: '))
+        load_data(input('Введите путь к файлу: '))
     )
     show_words(words)
